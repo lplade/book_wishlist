0961b350d8cc02de637068c6eb82fda392b502f3
Larry Lade <lplade@users.noreply.github.com>
2017-01-19 16:26:17 -0600

    Revised date store format, formatted __str__() to reflect

--- book.py
+++ book.py
@@ -1,11 +1,13 @@
-class Book:
+import datetime
 
-    ''' Represents one book in a user's list of books'''
+class Book:
+    """ Represents one book in a user's list of books"""
 
     NO_ID = -1
-    NO_DATE = -1
+    NO_DATE = datetime.date(1970, 1, 1)  # this date means uninitialized
 
-    def __init__(self, title, author, read=False, book_id=NO_ID, date_read=NO_DATE):
+    def __init__(self, title, author, read=False,
+                 book_id=NO_ID, date_read=NO_DATE):
         """
         Default book is unread, and has no ID
         """
@@ -19,11 +21,10 @@ class Book:
         self.id = book_id
 
     def set_date_read(self, date_read=NO_DATE):
-        import datetime
-        if date_read == -1:
-            date = datetime.datetime.now()
+        if date_read == (1970, 1, 1):
+            date_read = datetime.date.today()
         else:
-            # TODO validate a passed date to be in datetime format
+            # TODO validate a passed date to be in datetime.date format
             # not used at this point
             pass
         self.date_read = date_read
@@ -31,7 +32,11 @@ class Book:
     def __str__(self):
         read_str = 'no'
         if self.read:
-            read_str = 'yes'
+            read_year = self.date_read.year()
+            read_month = self.date_read.month()
+            read_day = self.date_read.day()
+            template = "{}-{}-{}"
+            read_str = template.format(read_year, read_month, read_day)
 
         id_str = self.id
         if id == -1:
@@ -41,4 +46,5 @@ class Book:
         return template.format(id_str, self.title, self.author, read_str)
 
     def __eq__(self, other):
-        return self.title == other.title and self.author == other.author and self.read == other.read and self.id==other.id
+        return self.title == other.title and self.author == other.author and \
+               self.read == other.read and self.id == other.id
