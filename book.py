f6dfcb55c066dc7fc484f955fb5682aacb245f1c
RHA <laksdjf@lkasjdla.biz>
2017-01-19 23:39:00 -0600

    added rating attribute to book class

--- book.py
+++ book.py
@@ -3,13 +3,15 @@ class Book:
     ''' Represents one book in a user's list of books'''
 
     NO_ID = -1
+    UNRATED = -1
 
-    def __init__(self, title, author, read=False, id=NO_ID):
+    def __init__(self, title, author, read=False, id=NO_ID, rating=UNRATED):
         '''Default book is unread, and has no ID'''
         self.title = title
         self.author = author
         self.read = read
         self.id=id
+        self.rating=rating
 
 
     def set_id(self, id):
@@ -25,10 +27,15 @@ class Book:
         if id == -1:
             id_str = '(no id)'
 
-        template = 'id: {} Title: {} Author: {} Read: {}'
-        return template.format(id_str, self.title, self.author, read_str)
+        rating_str = self.rating
+        if self.rating == -1:
+            rating_str = 'Not Rated'
+
+        template = 'id: {} Title: {} Author: {} Read: {} Rating: {}'
+        return template.format(id_str, self.title, self.author, read_str, rating_str)
 
 
     def __eq__(self, other):
-        #return ((self.title, self.author, self.read, self.id) == (other.title,other.author,other.read,other.id))
-        return self.title == other.title and self.author == other.author and self.read == other.read and self.id==other.id
+        return ((self.title, self.author, self.read, self.id) == \
+                (other.title,other.author,other.read,other.id))
+        #return self.title == other.title and self.author == other.author and self.read == other.read and self.id==other.id
